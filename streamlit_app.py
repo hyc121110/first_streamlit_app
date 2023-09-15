7b18372554d44a6a1a5b2b478e03bd904c86ee70
Evan Chan <32519231+hyc121110@users.noreply.github.com>
2023-09-15 12:18:37 -0400

    Update streamlit_app.py
    removed preloaded option

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,14 +27,12 @@ streamlit.dataframe(fruits_to_show)
 # new seciton to display fruityvice api response 
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
     fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_choice}")
-    # normalize the json response
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    # put the normalized data into a dataframe
     streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
   streamlit.error()
